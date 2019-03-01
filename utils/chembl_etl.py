27c4479583b61c952c9379d560300439a5e14099
Ian McKenzie <sirreal.ian@gmail.com>
2019-03-01 13:26:54 -0700

    Add simple query to get data so I can work on classifier

--- utils/chembl_etl.py
+++ utils/chembl_etl.py
@@ -3,6 +3,7 @@ This module will download the SQLite version of the ChEMBL database if it
 doesn't exist in ../data, and use it to create the data sets for analysis.
 """
 
+import csv
 import os
 import sqlite3
 import tarfile
@@ -19,9 +20,15 @@ class ChEMBL_SQLite(object):
     url = "ftp://ftp.ebi.ac.uk/pub/databases/chembl/ChEMBLdb/releases/chembl_24_1/"
     filename = "chembl_24_1_sqlite.tar.gz"
     dbpath = "chembl_24/chembl_24_sqlite/chembl_24.db"
+    csvfilename = "raw_data.csv"
 
     def __init__(self, path="../data/"):
         self.path = path
+
+    def get_raw_data(self):
+        """Will create the raw data if it does not already exist."""
+        if not os.path.isfile(self.path + self.csvfilename):
+            self._write_raw_data()
 
     def db_connect(self):
         """Returns a connection to the ChEMBL database,
@@ -35,6 +42,45 @@ class ChEMBL_SQLite(object):
 
         conn = sqlite3.connect(self.path + self.dbpath)
         return conn
+
+    def _write_raw_data(self):
+        """This runs the query to get our data from the database
+        For now this query returns good-enough data to do a quick analysis,
+        this is not the final query.
+        """
+
+        query = """
+                SELECT 
+                    canonical_smiles, published_value, published_units, pref_name
+                FROM 
+                    compound_structures as cs, activities as ac, assays as assays, target_dictionary as td
+                WHERE 
+                    cs.molregno = ac.molregno 
+                AND
+                    /* Endpoint is Ki*/
+                    ac.bao_endpoint = "BAO_0000192"
+                AND
+                    ac.published_value IS NOT NULL
+                AND
+                    ac.assay_id = assays.assay_id
+                AND
+                    assays.tid = td.tid
+                AND
+                    td.pref_name != "Unchecked"
+                AND
+                    td.organism = "Homo sapiens"
+                """
+
+        conn = self.db_connect()
+        print("Running SQL query")
+        cur = conn.execute(query)
+        headers = [x[0] for x in cur.description]
+        data_table = cur.fetchall()
+
+        with open(self.path + self.csvfilename, "w") as f:
+            writer = csv.writer(f)
+            writer.writerow(headers)
+            writer.writerows(data_table)
 
     def _download(self):
         """Downloads the ChEMBL database if it doesn't exist"""
